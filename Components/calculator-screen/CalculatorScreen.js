dec39c83bc56bab85cca1db7df5678639fd044c5
HAMZA NAWAB <hamzanawab.bs@gmail.com>
2022-08-03 02:41:39 +0500

    Calculator header completed

---
+++ Components/calculator-screen/CalculatorScreen.js
@@ -0,0 +1,9 @@
+const CalculatorScreen = () => {
+    return (
+        <div className="w-[35%] h-[15%] flex items-center justify-end mt-[20px] bg-screenbg_blue rounded-lg">
+            <p className="mr-[25px] text-[48px] text-text_white">399,981</p>
+        </div>
+    )
+}
+
+export default CalculatorScreen
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import CalculatorHead from "../Components/calculator-head/CalculatorHead"
+import CalculatorScreen from "../Components/calculator-screen/CalculatorScreen"
 
 export default function Home() {
   return (
-    <div className="bg-bgmain_blue h-[50rem] w-[100vw] flex justify-center items-center">
+    <div className="bg-bgmain_blue h-[60rem] w-[100vw] flex justify-center items-center flex-col">
       <CalculatorHead />
+      <CalculatorScreen />
     </div>
   )
 }
